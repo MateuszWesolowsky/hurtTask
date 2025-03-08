b4099e212ac08afc30ce1d96df990f9238419af2
Mateusz <m.wesolowski.92@gmail.com>
2025-03-08 11:10:43 +0100

    create api to fetch genres

---
+++ src/api/genresApi.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { GenresResponse } from "../types/types";
+
+const API_TOKEN = import.meta.env.VITE_BEARER_TOKEN;
+
+const genresApi = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+});
+
+export const fetchGenres = async (): Promise<GenresResponse[]> => {
+  const { data } = await genresApi.get(`/genre/movie/list`, {
+    headers: {
+      Authorization: `Bearer ${API_TOKEN}`,
+    },
+  });
+  return data.genres;
+};
