0a3ab28728e8ebffd49723bf5a81272b1ef2ef08
Mateusz <m.wesolowski.92@gmail.com>
2025-03-09 17:14:16 +0100

    remove styles

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,5 +1,5 @@
 export const ErrorMessage = () => (
-  <div className="bg-red-100 border w-fit border-red-400 text-red-700 px-4 py-3 rounded mx-auto flex justify-center items-center h-full">
+  <div className="bg-red-100 border w-fit border-red-400 text-red-700 px-4 py-3 rounded mx-auto">
     <strong className="font-bold">Błąd podczas pobierania danych!</strong>
   </div>
 );
