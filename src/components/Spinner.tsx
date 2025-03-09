1e6b079175d19185be39051f01561b5e4265227e
Mateusz <m.wesolowski.92@gmail.com>
2025-03-09 14:45:42 +0100

    create spinner component

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,5 @@
+export const Spinner = () => (
+  <div className="fixed inset-0 flex items-center justify-center">
+    <div className="w-12 h-12 border-4 border-blue-500 border-dotted rounded-full animate-spin"></div>
+  </div>
+);
