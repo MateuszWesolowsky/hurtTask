e890fe9da0038386914733e823f836200d6bebf6
Mateusz <m.wesolowski.92@gmail.com>
2025-03-09 14:52:03 +0100

    create converter function

---
+++ src/utils/convertToTime.ts
@@ -0,0 +1,7 @@
+export const convertToTime = (minutes: number) => {
+  const hours = Math.floor(minutes / 60);
+
+  const remainingMinutes = minutes % 60;
+
+  return `${hours}h ${remainingMinutes}m`;
+};
