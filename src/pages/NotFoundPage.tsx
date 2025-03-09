263a64d1e4d8bdfecb0260b2e4aa1ab5344ce1e2
Mateusz <m.wesolowski.92@gmail.com>
2025-03-09 14:52:39 +0100

    create notFoundPage

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+export const NotFoundPage = () => (
+  <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-center w-full">
+    <h1 className="text-5xl font-bold text-red-500">
+      404 - Strona nie znaleziona
+    </h1>
+    <p className="text-xl mt-4 text-gray-700">
+      Przepraszamy, strona, której szukasz, nie istnieje.
+    </p>
+    <Link to="/" className="mt-6 text-blue-500 hover:text-blue-700">
+      Powrót do strony głównej
+    </Link>
+  </div>
+);
