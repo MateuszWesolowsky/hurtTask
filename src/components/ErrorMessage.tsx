71127b55c33dd563e7af34c3faadf600956167cb
Mateusz <m.wesolowski.92@gmail.com>
2025-03-09 14:45:57 +0100

    create error message component

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,5 @@
+export const ErrorMessage = () => (
+  <div className="bg-red-100 border w-fit border-red-400 text-red-700 px-4 py-3 rounded mx-auto flex justify-center items-center h-full">
+    <strong className="font-bold">Błąd podczas pobierania danych!</strong>
+  </div>
+);
