034abe180d2f659dd00a087d6a0ca60049367ab3
Mateusz <m.wesolowski.92@gmail.com>
2025-03-09 17:14:46 +0100

    add styles and disabled option for selects

--- src/components/MovieFilters.tsx
+++ src/components/MovieFilters.tsx
@@ -21,37 +21,40 @@ export const MovieFilters = ({
   const { genres } = useFetchGenres();
 
   return (
-    <div className="flex gap-5 mb-6 p-2">
+    <div className="flex flex-col sm:flex-row gap-5 mb-6 p-4 justify-between min-w-full mt-16">
       <TextField
         label="Search"
         variant="outlined"
-        fullWidth
         margin="normal"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
-      <Select
-        displayEmpty
-        value={sortBy}
-        onChange={(e) => setSortBy(e.target.value)}
-      >
-        <MenuItem value="">Sort By</MenuItem>
-        <MenuItem value="popularity.desc">Most Popular</MenuItem>
-        <MenuItem value="release_date.desc">Newest</MenuItem>
-        <MenuItem value="vote_average.desc">Best Rated</MenuItem>
-      </Select>
-      <Select
-        displayEmpty
-        value={genre}
-        onChange={(e) => setGenre(e.target.value)}
-      >
-        <MenuItem value="">Select Genre</MenuItem>
-        {genres?.map((g) => (
-          <MenuItem key={g.id} value={g.id}>
-            {g.name}
-          </MenuItem>
-        ))}
-      </Select>
+      <div className="flex gap-3">
+        <Select
+          displayEmpty
+          disabled={!!search}
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}
+        >
+          <MenuItem value="">Sort By</MenuItem>
+          <MenuItem value="popularity.desc">Most Popular</MenuItem>
+          <MenuItem value="release_date.desc">Newest</MenuItem>
+          <MenuItem value="vote_average.desc">Best Rated</MenuItem>
+        </Select>
+        <Select
+          displayEmpty
+          disabled={!!search}
+          value={genre}
+          onChange={(e) => setGenre(e.target.value)}
+        >
+          <MenuItem value="">Select Genre</MenuItem>
+          {genres?.map((g) => (
+            <MenuItem key={g.id} value={g.id}>
+              {g.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </div>
     </div>
   );
 };
