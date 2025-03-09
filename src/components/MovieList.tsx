755ca30584ab6dba66fba28459fcce63d9eab999
Mateusz <m.wesolowski.92@gmail.com>
2025-03-09 17:18:26 +0100

    add link to card component, loading and error handling with Spinner and ErrorMessage components

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,27 +1,31 @@
-import { Grid2 as Grid } from "@mui/material";
-import MovieCard from "./MovieCard";
+import { MovieCard } from "./MovieCard";
 import { Movie } from "../types/types";
+import { Link } from "react-router-dom";
+import { Spinner } from "./Spinner";
+import { ErrorMessage } from "./ErrorMessage";
 
 interface Props {
   movies: Movie[];
+  isLoading: boolean;
+  isError: boolean;
 }
 
-const MovieList = ({ movies }: Props) => {
-  console.log(movies);
-  return (
-    <div className="grid grid-cols-5 gap-3">
-      {movies?.map((movie) => (
-        <Grid key={movie.id} sx={{ width: 250 }}>
-          <MovieCard movie={movie} />
-        </Grid>
-      ))}
+const MovieList = ({ movies, isLoading, isError }: Props) => {
+  if (isLoading) {
+    return <Spinner />;
+  }
 
-      {/* Paginacja */}
-      {/* <MoviePagination
-        page={page}
-        setPage={setPage}
-        totalPages={data.total_pages}
-        /> */}
+  if (isError) {
+    return <ErrorMessage />;
+  }
+
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-7 gap-x-3 gap-y-5 p-4">
+      {movies?.map((movie) => (
+        <Link to={`/movie/${movie.id}`} key={movie.id}>
+          <MovieCard movie={movie} />
+        </Link>
+      ))}
     </div>
   );
 };
