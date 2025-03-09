11863cd4946ca5683cf82aabea6d154ced740c7a
Mateusz <m.wesolowski.92@gmail.com>
2025-03-09 17:15:28 +0100

    add raiting and fixed styles

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardMedia, CardContent, Typography } from "@mui/material";
 import { Movie } from "../types/types";
+import { raitingColors } from "../utils/raitingColors";
 
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
@@ -7,27 +8,28 @@ interface Props {
   movie: Movie;
 }
 
-const MovieCard = ({ movie }: Props) => {
-  // console.log(movie);
-  return (
-    <Card className="h-full flex flex-col">
-      <CardMedia
-        sx={{ height: 350 }}
-        component="img"
-        // height="140"
-        image={`${
-          !movie.poster_path
-            ? "https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            : IMAGE_BASE_URL + movie.poster_path
-        }`}
-        alt={movie.title}
-      />
-      <CardContent className="flex-grow overflow-auto">
-        <Typography variant="h6">{movie.title}</Typography>
-        <Typography variant="body2">Rating: {movie.vote_average}</Typography>
-      </CardContent>
-    </Card>
-  );
-};
-
-export default MovieCard;
+export const MovieCard = ({ movie }: Props) => (
+  <Card className="h-full flex flex-col cursor-pointer rounded-xl shadow-lg hover:scale-105 transition-transform duration-200">
+    <CardMedia
+      className="h-full"
+      component="img"
+      image={`${
+        !movie.poster_path
+          ? "https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          : IMAGE_BASE_URL + movie.poster_path
+      }`}
+      alt={movie.title}
+    />
+    <CardContent>
+      <Typography variant="h6" className="text-sm font-semibold">
+        {movie.title}
+      </Typography>
+      <Typography variant="body2">
+        Rating:{" "}
+        <span className={`${raitingColors(movie.vote_average)}`}>
+          {movie.vote_average.toFixed(1)}
+        </span>
+      </Typography>
+    </CardContent>
+  </Card>
+);
