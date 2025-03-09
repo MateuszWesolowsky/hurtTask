139184aafa6b60bb289733d11d9ce81f421ce8cd
Mateusz <m.wesolowski.92@gmail.com>
2025-03-09 14:56:14 +0100

    add routing to application

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,30 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { NotFoundPage } from "./pages/NotFoundPage.tsx";
+import { MovieDetailPage } from "./pages/MovieDetailPage.tsx";
 
 const queryClient = new QueryClient();
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    path: "/",
+  },
+  {
+    element: <MovieDetailPage />,
+    path: "/movie/:id",
+  },
+  {
+    element: <NotFoundPage />,
+    path: "*",
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>
 );
