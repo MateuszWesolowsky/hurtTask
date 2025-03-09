c2f3d7520f7adece2c594eb9432f56455d3f9960
Mateusz <m.wesolowski.92@gmail.com>
2025-03-09 14:42:52 +0100

    create animation for detail element

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "slide-in-left": "slideInLeft 1s ease-out 0.5s backwards",
+      },
+      keyframes: {
+        slideInLeft: {
+          "0%": {
+            transform: "translateX(-100%)",
+            opacity: 0,
+          },
+          "100%": {
+            transform: "translateX(0)",
+            opacity: 1,
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
